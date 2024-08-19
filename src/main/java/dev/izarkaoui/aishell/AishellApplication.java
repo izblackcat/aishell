dd04aedb43e1f05886dcf4011da8a357fd15e743
izarkaoui <imzarkaoui@gmail.com>
2024-08-19 12:06:58 +0100

    initial commit

---
+++ src/main/java/dev/izarkaoui/aishell/AishellApplication.java
@@ -0,0 +1,13 @@
+package dev.izarkaoui.aishell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AishellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AishellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/izarkaoui/aishell/AishellApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.izarkaoui.aishell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AishellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
